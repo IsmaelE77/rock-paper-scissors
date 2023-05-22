cf3d7a36f0e6c46ec03d524b094289f6773dde5a
IsmaelE77 <ismael.ka@protonmail.com>
2023-05-22 15:24:55 +0300

    making the initial files then write the method to get computer choice

---
+++ script.js
@@ -0,0 +1,17 @@
+const ROCK = 1;
+const PAPER = 2;
+const SCISSORS = 3;
+function getComputerChoice(){
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch(randomNumber){
+        case 1 :
+            return ROCK;
+            break;
+        case 2 :
+            return PAPER;
+            break;
+        case 3 :
+            return SCISSORS;
+            break;    
+    }
+}
