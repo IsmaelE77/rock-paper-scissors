b4c6f335c2c749a1cad6af562abf945edf7b9964
IsmaelE77 <ismael.ka@protonmail.com>
2023-05-30 01:03:52 +0300

    أنهيت ربط واجهة الترحيب بالكود وأضفت مؤقت بسيط من اجل اظهار تأثر اختيار المستخدم

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ computer = 0;
 player = 0;
 tie = 0;
 
+const wait = ms => new Promise(res => setTimeout(res, ms));
+
 function getComputerChoice(){
     randomNumber = Math.floor(Math.random() * 3) + 1;
     switch(randomNumber){
@@ -22,7 +24,7 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function selectWinner(playerSelection, computerSelection) {
     if(computerSelection == playerSelection)
         return tie++;
     else if(playerSelection == ROCK)
@@ -34,24 +36,61 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-
 const choices = document.querySelectorAll('.player-choice .choice');
-choices.forEach(choice => choice.addEventListener('click',play));
+choices.forEach(choice => choice.addEventListener('click', e => playRound(e)));
 
-
-function play(e) {
+function playRound(e) {
     e.target.classList.add('focus');
-    playRound(e.target.dataset.key,getComputerChoice());
+    selectWinner(e.target.dataset.key,getComputerChoice());
+    round++;
     updateStatus();
-  }
+    setTimeout(function() {
+        e.target.classList.remove('focus');
+        welcome();
+    }, 1000);
+}
 
-  const roundCounter = document.querySelector('.round');
-  const computerScore = document.querySelector('.computerScore');
-  const playerScore = document.querySelector('.playerScore');
-  const tieCounter = document.querySelector('.tie');
-  function updateStatus(){
+
+const roundCounter = document.querySelector('.round');
+const computerScore = document.querySelector('.computerScore');
+const playerScore = document.querySelector('.playerScore');
+const tieCounter = document.querySelector('.tie');
+
+function updateStatus(){
     roundCounter.textContent = `round : ${round}`;
     computerScore.textContent = `computer score : ${computer}`;
     playerScore.textContent = `your score : ${player}`;
     tieCounter.textContent = `tie : ${tie}`;
-  }
+}
+
+const gameBoard = document.querySelector('.gameBoard');
+const welcomeBoard = document.querySelector('.welcomeBoard');
+
+const newRoundBtn = document.querySelector('#round');
+const startBtn = document.querySelector('#start');
+
+startBtn.addEventListener('click',newGame);
+newRoundBtn.addEventListener('click',continueGame);
+
+function newGame(){
+    round = 0 ;
+    computer = 0 ;
+    player = 0;
+    tie = 0;
+    updateStatus();
+    welcomeBoard.classList.add('hidden');
+    gameBoard.classList.remove('hidden');
+}
+
+function continueGame(){
+    welcomeBoard.classList.add('hidden');
+    gameBoard.classList.remove('hidden');
+}
+
+function welcome(){
+    gameBoard.classList.add('hidden');
+    welcomeBoard.classList.remove('hidden');
+    startBtn.textContent = "Start From Over";
+    newRoundBtn.classList.remove('hidden');
+}
+
