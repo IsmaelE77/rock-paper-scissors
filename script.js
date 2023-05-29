a2c27f87f11fab1cd7d3d2f281109a6e96edbdf1
IsmaelE77 <ismael.ka@protonmail.com>
2023-05-29 23:16:42 +0300

    أنهيت التصميم بشكل كامل كتبت الكود المسؤول جلب خيار المستخدم وتحديث النتائج

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
+round = 0 ;
+computer = 0;
+player = 0;
+tie = 0;
+
 function getComputerChoice(){
     randomNumber = Math.floor(Math.random() * 3) + 1;
     switch(randomNumber){
@@ -19,11 +24,34 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection) {
     if(computerSelection == playerSelection)
-        return "tie";
+        return tie++;
     else if(playerSelection == ROCK)
-        return computerSelection==PAPER ? "computer": "player";   
+        return computerSelection == PAPER ? computer++: player++;   
     else if(playerSelection == PAPER)
-        return computerSelection == SCISSORS ? "computer": "player";   
+        return computerSelection == SCISSORS ? computer++: player++;   
     else
-        return computerSelection==ROCK ? "computer": "player"; 
+        return computerSelection == ROCK ? computer++: player++; 
 }
+
+
+
+const choices = document.querySelectorAll('.player-choice .choice');
+choices.forEach(choice => choice.addEventListener('click',play));
+
+
+function play(e) {
+    e.target.classList.add('focus');
+    playRound(e.target.dataset.key,getComputerChoice());
+    updateStatus();
+  }
+
+  const roundCounter = document.querySelector('.round');
+  const computerScore = document.querySelector('.computerScore');
+  const playerScore = document.querySelector('.playerScore');
+  const tieCounter = document.querySelector('.tie');
+  function updateStatus(){
+    roundCounter.textContent = `round : ${round}`;
+    computerScore.textContent = `computer score : ${computer}`;
+    playerScore.textContent = `your score : ${player}`;
+    tieCounter.textContent = `tie : ${tie}`;
+  }
